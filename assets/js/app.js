3b198fdd1f742f8646d4d40d572b8efa580d4fcb
Sébastien ORTEGA <ortega.sebastien@gmail.com>
2019-04-28 19:28:44 +0200

    Revers icons by group

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,8 +50,8 @@ function animView(){
     disable:  (window.innerWidth < 768 || $('#map').length>0 || $('div[data-aos]').length<=6),
     once: true,
   });
+  reverseGoIcons();
 
-  animGo();
 
 }
 
@@ -67,6 +67,7 @@ function menuListener(){
 
 
   var toggleMenu = function(){
+    reverseMenuIcons();
     var opened = $('#menu').hasClass('open');
     $('#menu').toggleClass('open');
 
@@ -105,8 +106,79 @@ function menuListener(){
   $('#close-btn').click(toggleMenu);
 }
 
+
+
+var groupGo = 1;
+function reverseGoIcons(){
+  var htmlId = "#gcw-video";
+  var fadeItems = function(){
+    reverseIconsOfGroup(htmlId, groupGo);
+    groupGo++;
+    if(!$(htmlId+' .gcw-ico-g'+groupGo+'-1').hasClass('gcw-ico')){
+      groupGo=1;
+    }
+  }
+  setInterval(fadeItems,3500);
+}
+// icons within top menu
+var groupMenu = 1;
+function reverseMenuIcons(){
+  var htmlId = "#gcw-video-menu";
+  var fadeItems = function(){
+    reverseIconsOfGroup(htmlId, groupMenu);
+    groupMenu++;
+    if(!$(htmlId+' .gcw-ico-g'+groupMenu+'-1').hasClass('gcw-ico')){
+      groupMenu=1;
+    }
+  }
+  setInterval(fadeItems,3500);
+}
+
+function reverseIconsOfGroup(htmlId, groupIndex){
+  var icons = ['gocoworker','gocompany','gocobooster','gococampus'];
+  var iconCount = icons.length;
+  //  var faddingIcoSession = faddingIco;
+
+  $('.fadeIO.on').removeClass('on');
+
+
+
+  var rand = randomIndex(icons);
+  var rand2 = rand;
+  while(rand2==rand){
+    rand2 = randomIndex(icons);
+  }
+  console.log(rand, rand2);
+  var icoKlassIndex1 = htmlId+' .gcw-ico-g'+groupIndex+'-'+(rand);
+  var icoKlassIndex2 = htmlId+' .gcw-ico-g'+groupIndex+'-'+(rand2);
+  console.log("fadding",icoKlassIndex1, icoKlassIndex2);
+  //reverse icons
+  var icoKlassName1 = false;
+  var icoKlassName2 = false;
+
+  for(var i = 0; i<iconCount ; i++)
+  {
+    if($(icoKlassIndex1).hasClass('gcw-ico-'+icons[i])){
+      icoKlassName1 = htmlId+' gcw-ico-'+icons[i];
+    }
+    if($(icoKlassIndex2).hasClass('gcw-ico-'+icons[i])){
+      icoKlassName2 = htmlId+' gcw-ico-'+icons[i];
+    }
+  }
+
+  $(icoKlassIndex1).addClass('on');
+  $(icoKlassIndex2).addClass('on');
+
+  setTimeout(function(){
+    console.log("revert",icoKlassName1, icoKlassName2);
+    $(icoKlassIndex1).removeClass(icoKlassName1).addClass(icoKlassName2);
+    $(icoKlassIndex2).removeClass(icoKlassName2).addClass(icoKlassName1);
+  }, 1500);
+}
+
 function animGo(){
   var fadeItems = function(){
+
     if($('.fadeIO.off').length>0){
       $('.fadeIO.off').removeClass('off');
     }
